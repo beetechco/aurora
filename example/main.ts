44efa6918f5d34ae922ed8ace7a01448b1d73356
Rafael Azuero Hurtado <razueroh@gmail.com>
2018-12-29 12:26:20 -0500

    Add example with a custom container view

--- example/main.ts
+++ example/main.ts
@@ -3,9 +3,13 @@ import * as ReactDOM from 'react-dom';
 import { ButtonModel } from 'ui/widget/button/ButtonModel';
 import { ButtonView } from 'ui/view/button/ButtonView';
 import { CustomButtonView } from './ui/view/button/CustomButtonView';
+import { ContainerModel } from 'ui/widget/container/ContainerModel';
+import { ContainerView } from 'ui/view/container/ContainerView';
+import { CustomContainerView } from './ui/view/container/CustomContainerView';
 import ActionListener from 'ui/component/common/ActionListener';
 import UIRegistry from 'ui/UIRegistry';
 import { Button } from 'ui/widget/button/Button';
+import { Container } from 'ui/widget/container/Container';
 
 declare global {
   namespace NodeJS {
@@ -21,7 +25,7 @@ const dom = new JSDOM.JSDOM(`
 <!doctype html>
 <html>
   <body>
-    <div id ='root'>
+    <div id='root'>
     <div>
   </body>
 </html>
@@ -31,9 +35,18 @@ global.document = dom.window.document;
 global.window = dom.window;
 
 //Register UI Views
-UIRegistry.register(Button.UICODE as string, (id: string, model: any) => {
-  return new ButtonView(id, model);
-});
+UIRegistry.register(
+  Button.UICODE as string,
+  (id: string, model: any) => {
+    return new CustomButtonView(id, model);
+  });
+
+UIRegistry.register(
+  Container.UICODE as string,
+  (id: string, model: any) => {
+    return new CustomContainerView(id, model);
+  }
+);
 
 // create listener
 const actionListener = new class implements ActionListener {
@@ -65,3 +78,60 @@ const button = dom.window.document.getElementById(
   `${buttonView.getId()}:button`
 );
 button.click();
+
+/* Container */
+
+// create container model
+const containerModel = new ContainerModel();
+
+// create container view
+const containerView = new CustomContainerView('container', containerModel);
+
+// render
+ReactDOM.render(
+  containerView.paint(),
+  dom.window.document.getElementById('root')
+);
+console.log(dom.window.document.body.innerHTML);
+
+// create component
+const buttonComponent = new Button('buttoncomponent');
+buttonComponent.setModel(buttonModel);
+// render
+ReactDOM.render(
+  buttonComponent.getUI().paint(),
+  dom.window.document.getElementById('root')
+);
+console.log(dom.window.document.body.innerHTML);
+
+// create component container
+const containerComponent = new Container('containercomponent');
+containerComponent.setModel(containerModel);
+
+const buttonComponentContainer = new Button('buttoncomponentcontainer');
+buttonComponentContainer.setModel(buttonModel);
+
+buttonComponentContainer.setParent(containerComponent);
+containerModel.addElement(buttonComponentContainer);
+
+// render
+ReactDOM.render(
+  containerComponent.getUI().paint(),
+  dom.window.document.getElementById('root')
+);
+console.log(dom.window.document.body.innerHTML);
+
+const buttonComponentContainer2 = new Button('buttoncomponentcontainer2');
+buttonComponentContainer2.setModel(buttonModel);
+
+buttonComponentContainer2.setParent(containerComponent);
+containerModel.addElement(buttonComponentContainer2);
+
+containerComponent.getUI().repaint();
+
+// render
+ReactDOM.render(
+  containerComponent.getUI().paint(),
+  dom.window.document.getElementById('root')
+);
+console.log(dom.window.document.body.innerHTML);
---
+++ example/ui/view/container/CustomContainerView.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import IView from 'ui/component/view/IView';
+import { ContainerView } from 'ui/view/container/ContainerView';
+import Component from 'ui/component/Component';
+
+export class CustomContainerView extends ContainerView {
+  renderComponent = () => {
+    return (
+      <ContainerComponent
+        ref={this.elementRef}
+        id={this.getId()}
+        elements={
+          this.model
+            .getElements()
+            .map((component: Component<any>) =>
+              component.getUI()
+            )
+        }
+      />
+    );
+  }
+}
+
+interface IContainerComponentState {
+  elements: Array<IView<any>>;
+}
+
+interface IContainerComponentProps {
+  id: string;
+  elements: Array<IView<any>>;
+}
+
+export class ContainerComponent extends React.Component<
+  IContainerComponentProps,
+  IContainerComponentState
+  > {
+  constructor(props: IContainerComponentProps) {
+    super(props);
+    this.state = { 
+      elements: this.props.elements,
+    };
+  }
+
+  changeElements = (elements: Array<IView<any>>) => {
+    this.setState({ 
+      elements,
+    });
+  };
+
+  render() {
+    return (
+      <div 
+        id={`${this.props.id}:container`}
+        className='custom-container'
+      >
+        {
+          (this.state.elements || [])
+            .map((view: IView<any>) => (
+              <div key={`${this.props.id}:container:${view.getId()}`}>
+                {view.paint()}
+              </div>
+            ))
+        }
+      </div>
+    );
+  }
+}
