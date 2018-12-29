88b988e6a775061fff735d27dc45f8467698d3fb
Rafael Azuero Hurtado <razueroh@gmail.com>
2018-12-29 10:30:40 -0500

    Update example with custom button

--- example/main.ts
+++ example/main.ts
@@ -1,14 +1,11 @@
 import * as JSDOM from "jsdom";
 import * as ReactDOM from "react-dom";
-import ButtonModel from "ui/widget/button/ButtonModel";
-import ButtonView from "./ui/view/button/ButtonView";
+import { ButtonModel } from "ui/widget/button/ButtonModel";
+import { ButtonView } from "ui/view/button/ButtonView";
+import { CustomButtonView } from "./ui/view/button/CustomButtonView";
 import ActionListener from "ui/component/common/ActionListener";
-import ContainerView from "./ui/view/container/ContainerView";
-import ContainerModel from "ui/widget/container/ContainerModel";
 import UIRegistry from "ui/UIRegistry";
-import Button from "ui/widget/button/Button";
-import IView from "ui/component/view/IView";
-import ContainerComponent from "ui/widget/container/ContainerComponent";
+import { Button } from "ui/widget/button/Button";
 
 declare global {
   namespace NodeJS {
@@ -34,20 +31,13 @@ global.document = dom.window.document;
 global.window = dom.window;
 
 //Register UI Views
-UIRegistry.register(Button.UICODE as string, (id: String, model: any) => {
+UIRegistry.register(Button.UICODE as string, (id: string, model: any) => {
   return new ButtonView(id, model);
 });
 
-UIRegistry.register(
-  ContainerComponent.UICODE as string,
-  (id: String, model: any) => {
-    return new ContainerView(id, model);
-  }
-);
-
 // create listener
 const actionListener = new class implements ActionListener {
-  public actionPerformed(): void {
+  actionPerformed(): void {
     console.log("button action");
   }
 }();
@@ -57,8 +47,8 @@ const buttonModel = new ButtonModel();
 buttonModel.setLabel("Label 1");
 buttonModel.addActionListener(actionListener);
 
-// create button view
-const buttonView = new ButtonView("button", buttonModel);
+// create custom button view
+const buttonView = new CustomButtonView("button", buttonModel);
 console.log("button:", buttonModel, buttonView);
 
 // render
@@ -75,58 +65,3 @@ const button = dom.window.document.getElementById(
   `${buttonView.getId()}:button`
 );
 button.click();
-
-// create container model
-const containerModel = new ContainerModel();
-
-// create container view
-const containerView = new ContainerView("container", containerModel);
-
-// render
-ReactDOM.render(
-  containerView.paint(),
-  dom.window.document.getElementById("root")
-);
-console.log(dom.window.document.body.innerHTML);
-
-// create component
-const buttonComponent = new Button("buttoncomponent");
-buttonComponent.setModel(buttonModel);
-// render
-ReactDOM.render(
-  buttonComponent.getUI().paint(),
-  dom.window.document.getElementById("root")
-);
-console.log(dom.window.document.body.innerHTML);
-
-// create component container
-const containerComponent = new ContainerComponent("containercomponent");
-containerComponent.setModel(containerModel);
-
-const buttonComponentContainer = new Button("buttoncomponentcontainer");
-buttonComponentContainer.setModel(buttonModel);
-
-buttonComponentContainer.setParent(containerComponent);
-containerModel.addElement(buttonComponentContainer);
-
-// render
-ReactDOM.render(
-  containerComponent.getUI().paint(),
-  dom.window.document.getElementById("root")
-);
-console.log(dom.window.document.body.innerHTML);
-
-const buttonComponentContainer2 = new Button("buttoncomponentcontainerdos");
-buttonComponentContainer2.setModel(buttonModel);
-
-buttonComponentContainer2.setParent(containerComponent);
-containerModel.addElement(buttonComponentContainer2);
-
-containerComponent.getUI().repaint();
-
-// render
-ReactDOM.render(
-  containerComponent.getUI().paint(),
-  dom.window.document.getElementById("root")
-);
-console.log(dom.window.document.body.innerHTML);
---
+++ example/ui/view/button/CustomButtonView.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { ButtonView } from "ui/view/button/ButtonView";
+import { ButtonModel } from "ui/widget/button/ButtonModel";
+
+export class CustomButtonView extends ButtonView {
+  constructor(id: string, model: ButtonModel) {
+    super(id, model);
+  }
+  
+  renderComponent = () => {
+    return (
+      <ButtonComponent
+        ref={this.elementRef}
+        id={this.getId()}
+        label={this.model.getLabel()}
+        onAction={this.onAction}
+      />
+    );
+  }
+}
+
+interface IButtonComponentState {
+  label: string;
+}
+
+interface IButtonComponentProps {
+  id: string;
+  label: string;
+  onAction: Function;
+}
+
+class ButtonComponent extends React.Component<
+  IButtonComponentProps,
+  IButtonComponentState
+  > {
+  constructor(props: IButtonComponentProps) {
+    super(props);
+    this.state = {
+      label: this.props.label
+    };
+  }
+
+  changeLabel = (label: string): void => {
+    this.setState({
+      label
+    });
+  };
+
+  private onClick = () => {
+    if (this.props.onAction) {
+      this.props.onAction();
+    }
+  };
+
+  render() {
+    return (
+      <div id={`${this.props.id}:button`} onClick={this.onClick}>
+        {this.state.label}
+      </div>
+    );
+  }
+}
