c3e56ed503ebbe8cdd575ee4322d8918b7b735f1
Rafael Azuero Hurtado <razueroh@gmail.com>
2018-12-31 17:51:39 -0500

    Add tests to Button.ts

---
+++ src/ui/widget/button/__tests__/Button.test.ts
@@ -0,0 +1,81 @@
+import { Component } from 'ui/component/Component';
+import { Button } from '../Button';
+import { ButtonModel } from '../ButtonModel';
+import { ButtonView } from 'ui/view/button/ButtonView';
+import { UIRegistry } from 'ui/UIRegistry';
+
+describe('test button', () => {
+  it('should get the UICODE', () => {
+    const button = new Button('button');
+
+    expect(button.getUICode()).toBe('BUTTON');
+  });
+});
+
+describe('test component inheritance', () => {
+  let button: Button;
+
+  beforeEach(() => {
+    button = new Button('button');
+    jest.resetModules();
+  });
+
+  it('should set/get model', () => {
+    const buttonModel = new ButtonModel();
+
+    button.setModel(buttonModel);
+
+    expect(button.getModel()).toBe(buttonModel);
+  });
+
+
+  it('should set/get parent', () => {
+    const parent = new Component('parent');
+
+    button.setParent(parent);
+
+    expect(button.getParent()).toBe(parent);
+  });
+
+  describe('getUI', () => {
+    it('should get null if the component has no view and is not registered', () => {
+      expect(button.getUI()).toBeNull();
+    });
+
+    it('should get the view if component is registered', () => {
+      let buttonView;
+
+      UIRegistry.register(
+        Button.UICODE,
+        (model: ButtonModel, id: string) => {
+          buttonView = new ButtonView(model, id);
+
+          return buttonView;
+        }
+      );
+
+      expect(button.getUI()).toBe(buttonView);
+    });
+  });
+
+  it('should call repaint', () => {
+    const buttonModel = new ButtonModel();
+    const buttonView = new ButtonView(buttonModel);
+
+    UIRegistry.register(
+      Button.UICODE,
+      () => buttonView
+    );
+
+    const spy = jest.spyOn(buttonView, 'repaint');
+
+    button.getUI();
+    button.refreshUI();
+
+    expect(spy).toHaveBeenCalled();
+
+    spy.mockRestore();
+  });
+});
+
+
